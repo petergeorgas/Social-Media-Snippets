d624336c8ce0f50ec428eed3b1d780efb43a62f4
Peter Georgas <peter_georgas@yahoo.com>
2021-12-22 23:27:55 -0500

    Added tweet component props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Tweet from "./components/Tweet";
 function App() {
+  const [tweet, setTweet] = useState({});
+
+  useEffect(() => {
+    fetch("https://api.twitter.com/2/tweets?ids=1359729466721898498");
+  });
+
   return (
     <div className="App">
-      <Tweet></Tweet>
+      <Tweet
+        name="Peter Georgas"
+        handle="@peter_georgas"
+        pfp_link="https://pbs.twimg.com/profile_images/1399483683317428226/BkPY6kfN_400x400.jpg"
+        tweet_body="Hello! I am a nice Tweet!"
+        replies="45"
+        retweets="10.1K"
+        likes="15.8K"
+      ></Tweet>
     </div>
   );
 }
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -2,47 +2,67 @@ import React from "react";
 import "./Tweet.css";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-function Tweet() {
+import PropTypes from "prop-types";
+function Tweet(props) {
+  const {
+    name,
+    handle,
+    pfp_link,
+    tweet_body,
+    timestamp,
+    replies,
+    retweets,
+    likes,
+  } = props;
+
   return (
     <div>
       <div className="tweet-box">
         <div className="profile-info">
-          <img
-            src="https://pbs.twimg.com/profile_images/1399483683317428226/BkPY6kfN_400x400.jpg"
-            className="pfp"
-          />
+          <img src={pfp_link} className="pfp" />
           <div className="profile-name-container">
-            <h3 style={{ marginTop: 0, marginBottom: "5px" }}>Peter Georgas</h3>
+            <div className="profile-name-verify">
+              <h3 style={{ marginTop: 0, marginBottom: "5px" }}>{name}</h3>
+              <img
+                id="verify-badge"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Twitter_Verified_Badge.svg/640px-Twitter_Verified_Badge.svg.png"
+              />
+            </div>
             <p style={{ marginTop: 0, marginBottom: 0, color: "#777674" }}>
-              @peter_georgas
+              {handle}
             </p>
           </div>
           <FontAwesomeIcon id="twitter-logo" icon={faTwitter} size="3x" />
         </div>
         <div className="tweet-body">
-          <p style={{ marginBottom: 0 }}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare
-            quam vive
-          </p>
+          <p style={{ marginBottom: 0 }}>{tweet_body}</p>
         </div>
         <div className="timestamp">
           <p>10:41 PM • 9 Dec, 2021</p>
         </div>
         <div className="interactions-container">
           <p className="tweet-interaction-box">
-            <span className="tweet-interaction">1,551</span> replies
+            <span className="tweet-interaction">{replies}</span> replies
           </p>
           <p className="tweet-interaction-box">
-            <span className="tweet-interaction">10.1K</span> retweets
+            <span className="tweet-interaction">{retweets}</span> retweets
           </p>
           <p className="tweet-interaction-box">
-            <span className="tweet-interaction">4,892</span> likes
+            <span className="tweet-interaction">{likes}</span> likes
           </p>
         </div>
       </div>
     </div>
   );
 }
+
+Tweet.propTypes = {
+  name: PropTypes.string.isRequired,
+  handle: PropTypes.string.isRequired,
+  tweet_body: PropTypes.string.isRequired,
+  replies: PropTypes.string.isRequired,
+  retweets: PropTypes.string.isRequired,
+  likes: PropTypes.string.isRequired,
+};
 
 export default Tweet;
