e3ccb38136d17c0854bb0ae0482e0f582b8e2ac1
Peter Georgas <peter_georgas@yahoo.com>
2021-12-22 23:40:25 -0500

    Added state to Tweet link box

--- src/components/SnipLinkBox.js
+++ src/components/SnipLinkBox.js
@@ -1,18 +1,31 @@
-import React from 'react'
-import "./SnipLinkBox.css"
-
+import React, { useState } from "react";
+import "./SnipLinkBox.css";
 
 function SnipLinkBox() {
-    return (
-        <div className="snip-box-container">
-            <form>
-                <div id="snip-bar">
-                    <input type="text" className="link-box"placeholder="Paste a link to a tweet here" />
-                    <button type="submit" className="submit-button">Snip</button>
-                </div>
-            </form>
+  const [link, setLink] = useState(null); // Used to persist the Tweet link.
+
+  const onSnip = (event) => {
+    event.preventDefault();
+    console.log("Snip!");
+  };
+
+  return (
+    <div className="snip-box-container">
+      <form onSubmit={onSnip}>
+        <div id="snip-bar">
+          <input
+            type="text"
+            className="link-box"
+            placeholder="Paste a link to a tweet here"
+            onChange={(event) => setLink(event.target.value)}
+          />
+          <button type="submit" className="submit-button">
+            Snip
+          </button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
 
-export default SnipLinkBox
+export default SnipLinkBox;
