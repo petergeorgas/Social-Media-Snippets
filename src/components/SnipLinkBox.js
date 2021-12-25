2814a0dfbefa91172f01f5535def8a7afbcc35ec
Peter Georgas <peter_georgas@yahoo.com>
2021-12-25 11:34:31 -0500

    Added link validation, hooked into Next.js API backend deployed on Vercel.

--- src/components/SnipLinkBox.js
+++ src/components/SnipLinkBox.js
@@ -5,20 +5,40 @@ const tweet_status_regex = new RegExp(
   /^https?:\/\/twitter\.com\/(\w+)\/status\/(\d+)$/
 );
 
+const twitter_api_endpoint =
+  "https://social-media-snippets-functions.vercel.app/api/twitter";
+
 function SnipLinkBox() {
   const [link, setLink] = useState(undefined); // Used to persist the Tweet link.
+  const [invalidLink, setInvalidLink] = useState(false);
 
   const onSnip = (event) => {
     // Here, we are going to need to verify that the link is to a tweet.
     event.preventDefault();
 
-    if (tweet_status_regex.test(link)) {
+    const split_link = tweet_status_regex.exec(link);
+    // Match RegEx to make sure a Tweet link was input -- so we can properly generate a snippet.
+    if (split_link && split_link.length > 0) {
       console.log("Twitter status link has been entered!");
-      const id = link.split("/").at(-1);
-      console.log(`ID:${id}`);
+      if (invalidLink) setInvalidLink(false); // If invalid link was set to true, reset it.
+      const [link, account_name, status_id] = split_link;
+      const req = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: {
+          status_id: status_id,
+        },
+      };
+
+      fetch(twitter_api_endpoint, req).then((res) => {
+        console.log(res.json());
+      });
     } else {
       console.log("Not a Tweet status link.");
       // Display invalid link
+      setInvalidLink(true);
     }
   };
 
@@ -28,7 +48,7 @@ function SnipLinkBox() {
         <div id="snip-bar">
           <input
             type="text"
-            className="link-box"
+            className={invalidLink ? "link-box link-box-invalid" : "link-box"}
             placeholder="Paste a link to a tweet here"
             onChange={(event) => setLink(event.target.value)}
             value={link}
@@ -37,6 +57,7 @@ function SnipLinkBox() {
             Snip
           </button>
         </div>
+        {invalidLink ? <p id="invalid-link-label">Invalid Tweet link</p> : null}
       </form>
     </div>
   );
