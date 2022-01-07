7fed19209976438a72fa5f93393a1b389defbcdb
Peter Georgas <peter_georgas@yahoo.com>
2022-01-07 14:30:02 -0500

    Implemented initial image export

--- src/components/SnippetPreview.tsx
+++ src/components/SnippetPreview.tsx
@@ -1,82 +1,165 @@
-import React from 'react'
-import { useState } from 'react'
-import "./SnippetPreview.css"
-import {TweetURL, TweetHashtag, TweetMention} from "../utils/types/types"
-import {CirclePicker} from 'react-color'
+import React from "react";
+import { useState, useCallback, useRef } from "react";
+import { TweetURL, TweetHashtag, TweetMention } from "../utils/types/types";
+import { CirclePicker } from "react-color";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPalette, faDownload } from '@fortawesome/free-solid-svg-icons'
-import Tweet from './Tweet'
-import ExportButton from './ExportButton'
-import ExportOptions from './ExportOptions'
+import { faPalette, faDownload } from "@fortawesome/free-solid-svg-icons";
+import { toPng } from "html-to-image";
+import Tweet from "./Tweet";
+import ExportButton from "./ExportButton";
+import ExportOptions from "./ExportOptions";
+
+import "./SnippetPreview.css";
 
 type PreviewProps = {
-    tweet_name: string,
-    tweet_handle: string, 
-    tweet_verified?: boolean,
-    tweet_pfp_link: string,
-    tweet_urls?: Array<TweetURL>,
-    tweet_hashtags?: Array<TweetHashtag>
-    tweet_mentions?: Array<TweetMention>
-    tweet_body: string,
-    tweet_timestamp: Date,
-    tweet_replies: string,
-    tweet_retweets: string,
-    tweet_likes: string,
-}
- 
+	tweet_name: string;
+	tweet_handle: string;
+	tweet_verified?: boolean;
+	tweet_pfp_link: string;
+	tweet_urls?: Array<TweetURL>;
+	tweet_hashtags?: Array<TweetHashtag>;
+	tweet_mentions?: Array<TweetMention>;
+	tweet_body: string;
+	tweet_timestamp: Date;
+	tweet_replies: string;
+	tweet_retweets: string;
+	tweet_likes: string;
+};
+
 const SnippetPreview = (props: PreviewProps) => {
-    const {
-        tweet_name,
-        tweet_handle,
-        tweet_verified,
-        tweet_pfp_link,
-        tweet_urls,
-        tweet_hashtags,
-        tweet_mentions,
-        tweet_body,
-        tweet_timestamp,
-        tweet_replies,
-        tweet_retweets,
-        tweet_likes,
-      } = props;
+	const {
+		tweet_name,
+		tweet_handle,
+		tweet_verified,
+		tweet_pfp_link,
+		tweet_urls,
+		tweet_hashtags,
+		tweet_mentions,
+		tweet_body,
+		tweet_timestamp,
+		tweet_replies,
+		tweet_retweets,
+		tweet_likes,
+	} = props;
 
-    const [background_color, setBackgroundColor] = useState<string>("#03A9F4");
+	const [background_color, setBackgroundColor] = useState<string>("#03A9F4");
 
-    return (
-        <div>
-            <div className="preview-container-outer">
-                <div id="options-bar" style={{padding:"20px", marginLeft: "30px", marginRight: "30px"}}>
-                    <FontAwesomeIcon id="palette-icon" icon={faPalette} size="3x" style={{color: "#ccc"}}/>
-                    <div className="divider" style={{height: "48px"}}></div>
-                    <CirclePicker circleSpacing={4} circleSize={22} color={background_color} onChangeComplete={(color) => setBackgroundColor(color.hex)}/>
-                    <div style={{marginLeft: "auto", display: "flex", flexDirection:"row", alignItems:"center", justifyContent: "center"}}>
-                        <FontAwesomeIcon id="export-icon" icon={faDownload} size="3x" style={{color: "#ccc"}}/>
-                        <div className="divider" style={{height: "48px"}}></div>
-                        <ExportOptions/>
-                    </div>
-                    <ExportButton export_action={() => {}}/>
-                </div>
-                <div className="preview-container" style={{backgroundColor: background_color}}>
-                    <Tweet
-                    variant="light"
-                    name={tweet_name}
-                    handle={tweet_handle}
-                    verified={tweet_verified}
-                    pfp_link={tweet_pfp_link}
-                    tweet_body={tweet_body}
-                    tweet_urls={tweet_urls}
-                    tweet_hashtags={tweet_hashtags}
-                    tweet_mentions={tweet_mentions}
-                    timestamp={tweet_timestamp}
-                    replies={tweet_replies}
-                    retweets={tweet_retweets}
-                    likes={tweet_likes}
-                    />
-                </div>
-            </div>
-        </div>
-    )
-}
+	const [exportOptions, setExportOptions] = useState({
+		scale: 1,
+		format: "PNG",
+	});
 
-export default SnippetPreview
+	const ref = useRef<HTMLDivElement>(null);
 
+	const setExport = (obj: { scale: number; format: string }) => {
+		setExportOptions(obj);
+	};
+
+	const getColor = () => {
+		return background_color;
+	};
+
+	const onExportClick = useCallback(() => {
+		if (ref.current === null) {
+			return;
+		}
+
+		if (exportOptions.format === "PNG") {
+			var backgnd = getColor();
+
+			toPng(ref.current, {
+				cacheBust: true,
+				width: 1100,
+				height: 700,
+				pixelRatio: 1,
+				style: {
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "center",
+					justifyContent: "center",
+				},
+				backgroundColor: backgnd,
+			})
+				.then((dataURL) => {
+					const link = document.createElement("a");
+					link.download = "tweet.png";
+					link.href = dataURL;
+					link.click();
+				})
+				.catch((err) => {
+					console.log(err);
+				});
+		}
+	}, [ref]);
+
+	return (
+		<div>
+			<div className="preview-container-outer">
+				<div
+					id="options-bar"
+					style={{ padding: "20px", marginLeft: "30px", marginRight: "30px" }}
+				>
+					<FontAwesomeIcon
+						id="palette-icon"
+						icon={faPalette}
+						size="3x"
+						style={{ color: "#ccc" }}
+					/>
+					<div className="divider" style={{ height: "48px" }}></div>
+					<CirclePicker
+						circleSpacing={4}
+						circleSize={22}
+						color={background_color}
+						onChangeComplete={(color) => setBackgroundColor(color.hex)}
+					/>
+					<div
+						style={{
+							marginLeft: "auto",
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							justifyContent: "center",
+						}}
+					>
+						<FontAwesomeIcon
+							id="export-icon"
+							icon={faDownload}
+							size="3x"
+							style={{ color: "#ccc" }}
+						/>
+						<div className="divider" style={{ height: "48px" }}></div>
+						<ExportOptions onExportOptionsChange={setExport} />
+					</div>
+					<ExportButton export_action={onExportClick} />
+				</div>
+				<div>
+					{/* Nodes within here will be converted to image format. */}
+					<div
+						className="preview-container"
+						style={{ backgroundColor: background_color }}
+					>
+						<div ref={ref}>
+							<Tweet
+								variant="light"
+								name={tweet_name}
+								handle={tweet_handle}
+								verified={tweet_verified}
+								pfp_link={tweet_pfp_link}
+								tweet_body={tweet_body}
+								tweet_urls={tweet_urls}
+								tweet_hashtags={tweet_hashtags}
+								tweet_mentions={tweet_mentions}
+								timestamp={tweet_timestamp}
+								replies={tweet_replies}
+								retweets={tweet_retweets}
+								likes={tweet_likes}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default SnippetPreview;
--- src/utils/tweet_utils.ts
+++ src/utils/tweet_utils.ts
@@ -7,11 +7,11 @@ import DOMPurify from "dompurify";
  * @returns The link to the bigger (73x73 pixel) profile picture link.
  */
 export const getFullSizePfpLink = (small_icon_url: string): string => {
-  if (small_icon_url.includes("_normal.jpg")) {
-    return small_icon_url.replace("_normal.jpg", "_bigger.jpg");
-  } else {
-    throw Error("Provided URL did not contain _normal.jpg image suffix.");
-  }
+	if (small_icon_url.includes("_normal.")) {
+		return small_icon_url.replace("_normal.", "_bigger.");
+	} else {
+		throw Error("Provided URL did not contain _normal.<ext> image suffix.");
+	}
 };
 
 /**
@@ -19,56 +19,70 @@ export const getFullSizePfpLink = (small_icon_url: string): string => {
  * @ (mentions), # (hashtags), and URLs are highlighted. This function does the same
  * generating an HTML string. The HTML string is sanitized using DOMPurify before
  * being returned.
- * 
- * @param tweet_body The plaintext of the tweet, without any highlighting 
+ *
+ * @param tweet_body The plaintext of the tweet, without any highlighting
  * @param urls  An array of URLs contained within the tweet, returned by the Twitter API
  * @param hashtags An array of hashtags contained within the tweet, returned by the Twitter API
  * @param mentions An array of mentions contained within the tweet, returned by the Twitter API
  * @returns A sanitized HTML string, with correct styling applied for mentions, URLs, and hashtags
  */
-export const highlightTweetTags = (tweet_body: string, urls?: Array<TweetURL>, hashtags?: Array<TweetHashtag>, mentions?: Array<TweetMention>): string => {
-  tweet_body = `<p id="tweet-body-text">${tweet_body}</p>`
-  console.log(`Hashtags: ${hashtags}`)
-  if(urls) {
-    urls.forEach(url => {
-      tweet_body = tweet_body.replace(`${url.url}`, `<span class="tweet-tag">${url.url}</span>`)
-    })
-  }
+export const highlightTweetTags = (
+	tweet_body: string,
+	urls?: Array<TweetURL>,
+	hashtags?: Array<TweetHashtag>,
+	mentions?: Array<TweetMention>
+): string => {
+	tweet_body = `<p id="tweet-body-text">${tweet_body}</p>`;
+	console.log(`Hashtags: ${hashtags}`);
+	if (urls) {
+		urls.forEach((url) => {
+			tweet_body = tweet_body.replace(
+				`${url.url}`,
+				`<span class="tweet-tag">${url.url}</span>`
+			);
+		});
+	}
 
-  if(hashtags){
-    hashtags.forEach(hashtag => {
-      tweet_body = tweet_body.replace(`#${hashtag.tag}`, `<span class="tweet-tag">#${hashtag.tag}</span>`)
-    });
-  }
+	if (hashtags) {
+		hashtags.forEach((hashtag) => {
+			tweet_body = tweet_body.replace(
+				`#${hashtag.tag}`,
+				`<span class="tweet-tag">#${hashtag.tag}</span>`
+			);
+		});
+	}
 
-  if(mentions) {
-    mentions.forEach(mention => {
-      tweet_body = tweet_body.replace(`@${mention.username}`, `<span class="tweet-tag">@${mention.username}</span>`)
-    });
-  }
+	if (mentions) {
+		mentions.forEach((mention) => {
+			tweet_body = tweet_body.replace(
+				`@${mention.username}`,
+				`<span class="tweet-tag">@${mention.username}</span>`
+			);
+		});
+	}
 
-
-  return DOMPurify.sanitize(tweet_body)
-}
+	return DOMPurify.sanitize(tweet_body);
+};
 
 /**
  * "Prettifies" a number to make it follow Twitter's standard.
- * For example, 8512 turns into "8,512". 10419 turns into "10.4K". 
+ * For example, 8512 turns into "8,512". 10419 turns into "10.4K".
  * @param metric_number A normal number returned by Twitter API call
  * @returns A prettified string of the metric number, formatted properly.
-*/
+ */
 export const prettifyTweetMetric = (metric_number: number): string => {
-  
-  if(metric_number > 999999) { // 1,000,000 or more
-    return `${(metric_number/1e6).toFixed(1)}M`;
-  } else if(metric_number > 9999) { //  10,000 or more 
-    return `${(metric_number/1e3).toFixed(1)}K`;
-  } else if (metric_number > 999){ // Less than 10,000 -- we need to add one comma..
-    let metric_number_str = metric_number.toString();
-    return `${metric_number_str.slice(0,1)},${metric_number_str.slice(1)}`;
-  } else { // Less than 1,000 -- need not do anything.
-    return metric_number.toString();
-  }
-
-}
-
+	if (metric_number > 999999) {
+		// 1,000,000 or more
+		return `${(metric_number / 1e6).toFixed(1)}M`;
+	} else if (metric_number > 9999) {
+		//  10,000 or more
+		return `${(metric_number / 1e3).toFixed(1)}K`;
+	} else if (metric_number > 999) {
+		// Less than 10,000 -- we need to add one comma..
+		let metric_number_str = metric_number.toString();
+		return `${metric_number_str.slice(0, 1)},${metric_number_str.slice(1)}`;
+	} else {
+		// Less than 1,000 -- need not do anything.
+		return metric_number.toString();
+	}
+};
