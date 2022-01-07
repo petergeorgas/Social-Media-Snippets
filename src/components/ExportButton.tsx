d2ed90cb0071852ee9f8c5a71f9fc8578c7f5a29
Peter Georgas <peter_georgas@yahoo.com>
2022-01-07 02:32:37 -0500

    Prettier formatting

--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import "./styles/ExportButton.css"
+import React from "react";
+import "./styles/ExportButton.css";
 
 type ExportButtonProps = {
-    export_action: React.MouseEventHandler<HTMLButtonElement>,
-}
+	export_action: React.MouseEventHandler<HTMLButtonElement>;
+};
 
 const ExportButton = (props: ExportButtonProps) => {
-    
-    return (
-        <button className="export-btn" onClick={props.export_action}>Export</button>
-    )
-}
+	return (
+		<button className="export-btn" onClick={props.export_action}>
+			Export
+		</button>
+	);
+};
 
-export default ExportButton
+export default ExportButton;
--- src/components/SnippetCustomizer.tsx
+++ src/components/SnippetCustomizer.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const SnippetCustomizer = ({}) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+	return <div></div>;
+};
 
-export default SnippetCustomizer
+export default SnippetCustomizer;
